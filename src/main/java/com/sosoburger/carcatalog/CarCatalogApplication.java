ed9a4936fdec9010065b72d1a648965406465919
SosoBugre <netego@>
2023-08-03 17:08:45 +0300

    Initial commit

---
+++ src/main/java/com/sosoburger/carcatalog/CarCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.sosoburger.carcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarCatalogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sosoburger/carcatalog/view/CatalogView.java
@@ -0,0 +1,9 @@
+package com.sosoburger.carcatalog.view;
+
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.Route;
+
+@Route("")
+public class CatalogView extends VerticalLayout {
+
+}
